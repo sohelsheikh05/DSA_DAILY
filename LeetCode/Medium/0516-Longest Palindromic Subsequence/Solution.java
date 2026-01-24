ad9b8e27b035274256f28131fa52a91738b9e41c
Sohel Sheikh <sheikhsohel1007@gmail.com>
2026-01-24 20:40:14 +0530

    Leetcode 516. Longest Palindromic Subsequence Bottom up

--- LeetCode/Medium/0516-Longest Palindromic Subsequence/Solution.java
+++ LeetCode/Medium/0516-Longest Palindromic Subsequence/Solution.java
@@ -1,23 +1,45 @@
 
-//Recursion+Memoization
+// //Recursion+Memoization
+// class Solution {
+//     public int longestPalindromeSubseq(String s) {
+//         int n=s.length();
+//         int dp[][]=new int[n][n];
+//         for(int i=0;i<n;i++) Arrays.fill(dp[i],-1);
+//     return lps(s,0,n-1,dp);
+//     }
+//     int lps(String s,int i,int j,int dp[][]){
+//         if(i>j) return 0;
+//         if(i==j){
+//             return 1;
+//         }
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         if(s.charAt(i)==s.charAt(j))
+//           dp[i][j]=2+lps(s,i+1,j-1,dp);
+//         else{
+//         dp[i][j]=Math.max(lps(s,i+1,j,dp),lps(s,i,j-1,dp));
+//         }
+//     return dp[i][j];
+//     }
+// }
+
+
 class Solution {
     public int longestPalindromeSubseq(String s) {
         int n=s.length();
         int dp[][]=new int[n][n];
-        for(int i=0;i<n;i++) Arrays.fill(dp[i],-1);
-    return lps(s,0,n-1,dp);
-    }
-    int lps(String s,int i,int j,int dp[][]){
-        if(i>j) return 0;
-        if(i==j){
-            return 1;
+        for(int i=0;i<n;i++) dp[i][i]=1;
+        
+        for(int l=2;l<=n;l++){
+            for(int i=0;i+l-1<n;i++){
+                int j=i+l-1;
+                 if(s.charAt(i)==s.charAt(j) ){
+                    dp[i][j]=2+dp[i+1][j-1];
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i+1][j],dp[i][j-1]);
+                }
+            }
         }
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s.charAt(i)==s.charAt(j))
-          dp[i][j]=2+lps(s,i+1,j-1,dp);
-        else{
-        dp[i][j]=Math.max(lps(s,i+1,j,dp),lps(s,i,j-1,dp));
-        }
-    return dp[i][j];
+    return dp[0][n-1];
     }
 }
