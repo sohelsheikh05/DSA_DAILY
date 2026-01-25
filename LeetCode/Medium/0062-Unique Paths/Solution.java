242c6b493237a5a63c76dc6b963190692479a7cc
Sohel Sheikh <sheikhsohel1007@gmail.com>
2026-01-25 13:54:12 +0530

    0062. Unique Paths Time: 0 ms Space : 42.36 mb Bottom Up

--- LeetCode/Medium/0062-Unique Paths/Solution.java
+++ LeetCode/Medium/0062-Unique Paths/Solution.java
@@ -1,3 +1,5 @@
+//BOTTOM UP
+
 class Solution {
     public int uniquePaths(int m, int n) {
         int dp[][]=new int[m][n];
@@ -11,3 +13,23 @@ class Solution {
     return dp[m-1][n-1];
     }
 }
+
+//Recursion +memoization
+
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+//            int dp[][]=new int[m][n];
+//         for(int i=0;i<m;i++) Arrays.fill(dp[i],-1);
+//         return solve(m,n,0,0,dp);
+//     }
+//     int solve(int m,int n,int i, int j,int dp[][]){
+//         if(i==m-1 && j==n-1){
+//             return 1;
+//         }
+//         if(i>= m || i<0 || j>=n || j<0) return 0;
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         int right=solve(m,n,i,j+1,dp);
+//         int down=solve(m,n,i+1,j,dp);
+//         return dp[i][j]=right+down;
+//     }
+// }
