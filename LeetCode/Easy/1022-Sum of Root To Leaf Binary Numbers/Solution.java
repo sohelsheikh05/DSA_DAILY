602a4a1647ceb1a40794a43c15808e84346bea32
Sohel Sheikh <sheikhsohel1007@gmail.com>
2026-02-24 16:39:39 +0530

    Leetcode 1022. Sum of Root To Leaf Binary Numbers

---
+++ LeetCode/Easy/1022-Sum of Root To Leaf Binary Numbers/Solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root,0);
+    }
+    int dfs(TreeNode root,int current){
+        if(root==null) return 0;
+        current=current*2+root.val;
+        if(root.left==null && root.right==null) return current;
+        return  dfs(root.left,current)+dfs(root.right,current);
+    }
+}
