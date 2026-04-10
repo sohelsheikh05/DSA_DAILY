8d5b049d6b49f5ce5da0f23e0aa21753936dba9b
Sohel Sheikh <sheikhsohel1007@gmail.com>
2026-04-10 10:34:53 +0530

    Leetcode  73. Set Matrix Zeroes

---
+++ LeetCode/Medium/0073-Set Matrix Zeroes/Solution.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean[] zeroRows = new boolean[m];
+        boolean[] zeroCols = new boolean[n];
+
+  
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    zeroRows[i] = true;
+                    zeroCols[j] = true;
+                }
+            }
+        }
+
+
+        for (int i = 0; i < m; i++) {
+            if (zeroRows[i]) {
+                for (int j = 0; j < n; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+
+        for (int j = 0; j < n; j++) {
+            if (zeroCols[j]) {
+                for (int i = 0; i < m; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
